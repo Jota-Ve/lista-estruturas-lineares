ec5a41aedb170b25805233c8c49253036dde3281
João Vitor Barbosa <j.vs.b13@gmail.com>
2023-06-04 01:40:09 -0300

    Implementa empilha()

--- include/lista-teorica-estruturas-lineares/06.hpp
+++ include/lista-teorica-estruturas-lineares/06.hpp
@@ -7,7 +7,9 @@
 #ifndef LISTA_TEORICA_ESTRUTURAS_LINEARES_06_HPP_
 #define LISTA_TEORICA_ESTRUTURAS_LINEARES_06_HPP_
 
+#include <cstdint>
 #include <lista-teorica-estruturas-lineares/PilhaTAD.hpp>
+#include <vector>  //NOLINT
 
 class No {
  public:
@@ -19,12 +21,19 @@ class PilhaMin {
  public:
   // incluir variáveis necessárias
   No* ultimo;
+  std::vector<int8_t> dados;
   int N;
 
   int topo() { return this->ultimo->dado; }
-  int tamanho();
+
+  int tamanho() { return this->N; }
+
   int desempilha();
-  void empilha(int t);
+  void empilha(int t) {
+    No* novoNo = new No{.dado = t, .prox = this->ultimo};
+    this->ultimo = novoNo;
+    this->N++;
+  }
   int obterMinimo();
 
   // mais métodos auxiliares
@@ -36,6 +45,20 @@ class PilhaMin {
   void libera() {
     while (this->N) {
       this->desempilha();
+    }
+  }
+
+  void insereDado(int dado) {
+    if (dado <= this->dados.back()) {
+      this->dados.push_back(dado);
+      return;
+    }
+
+    for (int8_t i = 0; i < this->dados.size(); i++) {
+      if (this->dados.at(i) < dado) {
+        this->dados.insert(this->dados.begin() + i, dado);
+        return;
+      }
     }
   }
 };
